96f4776fe192ba92542f8d90712a306fd30a0be0
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2023-03-03 15:05:53 +0700

    Ubah logic check username

--- middlewares/checkDuplicate.middleware.js
+++ middlewares/checkDuplicate.middleware.js
@@ -2,19 +2,30 @@ const models = require('../models');
 const User = models.User;
 
 const checkUsername = async (req, res, next) => {
-  const username = await User.findOne({
-    username: req.body.username
-  });
+  try {
+    const checkUsername = await User.findOne({
+      where: {
+        username: req.body.username
+      }
+    });
 
-  if (username) {
+    if (checkUsername) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Sorry! Username is Already Exists'
+      });
+    }
+
+    next();
+  } catch (error) {
     return res.status(400).json({
       status: 'Failed',
       statusCode: 400,
-      message: 'Sorry, Username is Already Exists!'
+      message: 'Something Error!',
+      error: error
     });
   }
-
-  next();
 };
 
 module.exports = {
