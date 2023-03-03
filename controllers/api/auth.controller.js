856f97b3f4dfc1615f6300b5bfa313a08d5dc7d5
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2023-03-03 15:40:24 +0700

    Tambah logic untuk login

--- controllers/api/auth.controller.js
+++ controllers/api/auth.controller.js
@@ -39,6 +39,87 @@ class AuthController {
       });
     }
   };
+
+  // Function Generate JWT Token
+  static generateToken = (id, username, role) => {
+    // > Tangkap id, username, dan role untuk dijadikan payload
+    const payload = {
+      id, username, role
+    };
+
+    // > SECRET_KEY Dari .env file 
+    const SECRET_KEY = process.env.SECRET_KEY;
+
+    // > Buat token
+    // => JWT akan expired dalam 24 jam
+    const token = jwt.sign(payload, SECRET_KEY, {
+      expiresIn: 86400
+    });
+
+    return token;
+  }
+
+  static userLogin = async (username, password) => {
+    try {
+      const userDatabase = await User.findOne({
+        where: {
+          username
+        }
+      });
+
+      if (!userDatabase) {
+        return Promise.reject('Check Again Your Login Data!');
+      }
+
+      const checkPassword = await bcrypt.compare(password, userDatabase.password);
+
+      if (!checkPassword) {
+        return Promise.reject('Check Again Your Login Data!');
+      }
+
+      return Promise.resolve(userDatabase);
+    } catch (error) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Something Error!',
+        error: error
+      });
+    }
+  };
+
+  static loginWithJWT = async (req, res) => {
+    try {
+      // > Tangkap Username dan Password = 
+      const { username, password } = req.body;
+
+      // > Check Username dan Password 
+      // => Apakah username atau password sama dengan db
+      const checkLogin = await AuthController.userLogin(username, password);
+
+      // > Simpan id, username, dan role user
+      const idUser = checkLogin.dataValues.id;
+      const usernameUser = checkLogin.dataValues.username;
+      const roleUser = checkLogin.dataValues.role;
+
+      // > Generate token 
+      const generateToken = AuthController.generateToken(idUser, usernameUser, roleUser);
+
+      return res.status(200).json({
+        status: 'Success',
+        statusCode: 200,
+        message: 'Success Login!',
+        accessToken: generateToken
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Something Error!',
+        error: error
+      });
+    }
+  };
 };
 
 module.exports = {
