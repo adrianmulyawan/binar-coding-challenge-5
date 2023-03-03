c42020e779f603a148e12168c814f99403a27575
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2023-03-03 14:19:34 +0700

    Tambah error handling middleware didalam jwt http server

--- server-jwt.js
+++ server-jwt.js
@@ -14,6 +14,24 @@ app.use(express.urlencoded({
 
 app.use(router);
 
+// > Error Handling Middleware
+// # When error = 500 (Internal Server Error)
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    status: 'Failed',
+    statusCode: 500,
+    message: 'Oops! Something Error!'
+  })
+});
+// # When error = 404 (Not Found)
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: 'Failed',
+    statusCode: 404,
+    message: 'Sorry, End Point is Not Found!'
+  });
+});
+
 app.listen(port, () => {
   console.info(`${new Date()} | Express run in port ${port}`);
 });
