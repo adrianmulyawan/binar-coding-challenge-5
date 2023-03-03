8c09480f617d8e0ab0afe1d4d5179ecb2fd883ee
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2023-03-03 13:55:58 +0700

    Buat migrations dan model untuk tabel room

---
+++ migrations/20230303065338-create-room.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Rooms', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      room_code: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      user_id_1: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      user_id_2: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      start_fight: {
+        type: Sequelize.DATE
+      },
+      finish_fight: {
+        type: Sequelize.DATE
+      },
+      winner_user_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Rooms');
+  }
+};
