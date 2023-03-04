efe6d4aaff636ba7db24b112af94bc9e5b49f2ca
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2023-03-05 00:14:46 +0700

    Tambah logic untuk menampilkan detail permainan

--- controllers/api/admin/room.controller.js
+++ controllers/api/admin/room.controller.js
@@ -26,6 +26,40 @@ class RoomAdminController {
       });
     }
   };
+
+  static getDetailRoom = async (req, res) => {
+    try {
+      const room_code = req.params.room_code;
+      const data = await Room.findOne({
+        where: {
+          room_code
+        }, 
+        include: ['userId1', 'userId2', 'roomfights']
+      });
+
+      if (!data) {
+        return res.status(404).json({
+          status: 'Failed',
+          statusCode: 404,
+          message: 'Sorry Room is Not Found!'
+        });
+      }
+
+      return res.status(200).json({
+        status: 'Success',
+        statusCode: 200,
+        message: 'Data Room Found!',
+        data: data
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Error Found!',
+        error: error
+      });
+    }
+  };
 };
 
 module.exports = {
