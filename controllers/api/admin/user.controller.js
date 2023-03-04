08c494d8fe6516db71b2d14a1baca89395f427c4
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2023-03-04 20:48:12 +0700

    Tambah logic menampilkan detail user

--- controllers/api/admin/user.controller.js
+++ controllers/api/admin/user.controller.js
@@ -25,7 +25,37 @@ class UserController {
         error: error
       });
     }
-  }
+  };
+
+  static findUserById = async (req, res) => {
+    try {
+      const data = await User.findByPk(req.params.id, {
+        include: ['profile']
+      });
+
+      if (!data) {
+        return res.status(404).json({
+          status: 'Failed',
+          statusCode: 404,
+          message: 'Sorry User is Not Found!'
+        });
+      }
+
+      return res.status(200).json({
+        status: 'Success',
+        statusCode: 200,
+        message: 'Data User Found!',
+        data: data
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 'Failed',
+        statusCode: 400,
+        message: 'Error Found!',
+        error: error
+      });
+    }
+  };
 };
 
 module.exports = {
