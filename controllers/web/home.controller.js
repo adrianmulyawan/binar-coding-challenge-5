5cf8434dbc2b6646e481b82cc7d8863f8c149461
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2023-03-05 17:41:40 +0700

    Edit logic untuk menampilkan halaman game atau halaman utama sistem

--- controllers/web/home.controller.js
+++ controllers/web/home.controller.js
@@ -2,7 +2,7 @@ class HomeController {
   // > Method Handle Halaman Utama
   static getHomePage = (req, res) => {
     // console.log(req.session);
-    res.render('pages/index', {
+    return res.render('pages/index', {
       layout: 'layouts/main-layouts',
       title: 'Temple Run',
       username: req.user.dataValues.username,
@@ -12,7 +12,7 @@ class HomeController {
 
   // > Method Handle Halaman Game
   static getGamePage = (req, res) => {
-    res.render('pages/game', {
+    return res.render('pages/game', {
       layout: 'layouts/game-layouts',
       title: 'ROCK, PAPER, SCISSORS GAME',
       checkRole: req.user.dataValues.role
