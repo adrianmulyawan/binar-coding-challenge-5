2b34c9d1f72352ad1ccd27c6bc2f9de6f187571f
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-03-05 19:49:59 +0700

    Tambah error handling ketika halaman tidak ditemukan dan server error

--- server-local.js
+++ server-local.js
@@ -43,19 +43,19 @@ app.use(router);
 
 // > Error Handling Middleware
 // # When error = 500 (Internal Server Error)
-// app.use((err, req, res, next) => {
-//   res.status(500).render('pages/internal-server-error', {
-//     layout: 'layouts/error-handling-layouts',
-//     title: 'Something Error!'
-//   })
-// });
+app.use((err, req, res, next) => {
+  res.status(500).render('pages/internal-server-error', {
+    layout: 'layouts/error-handling-layouts',
+    title: 'Something Error!'
+  })
+});
 // # When error = 404 (Not Found)
-// app.use((req, res, next) => {
-//   res.status(404).render('pages/notfound', {
-//     layout: 'layouts/error-handling-layouts',
-//     title: 'Not Found!'
-//   });
-// });
+app.use((req, res, next) => {
+  res.status(404).render('pages/notfound', {
+    layout: 'layouts/error-handling-layouts',
+    title: 'Not Found!'
+  });
+});
 
 app.listen(port, () => {
   console.info(`${new Date()} | Express run in port ${port}`);
