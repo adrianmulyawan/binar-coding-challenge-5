28f7b5736b430b47d392ffd71dc874515e204b2c
Adrian Mulyawan <adrianmulyawan666@gmail.com>
2023-03-05 16:05:29 +0700

    Tambah passport untuk handle mvc

--- server-local.js
+++ server-local.js
@@ -5,6 +5,7 @@ const flash = require('express-flash');
 const morgan = require('morgan');
 const expressLayouts = require('express-ejs-layouts');
 const methodOverride = require('method-override');
+const passport = require('./lib/passport-local');
 const router = require('./routes/web');
 
 const app = express();
@@ -23,6 +24,10 @@ app.use(session({
   saveUninitialized: false
 }));
 
+// > Use passport-local.js
+app.use(passport.initialize());
+app.use(passport.session());
+
 // > User Package method-override
 router.use(methodOverride('_method'));
 
@@ -38,19 +43,19 @@ app.use(router);
 
 // > Error Handling Middleware
 // # When error = 500 (Internal Server Error)
-app.use((err, req, res, next) => {
-  res.status(500).render('pages/internal-server-error', {
-    layout: 'layouts/error-handling-layouts',
-    title: 'Something Error!'
-  })
-});
+// app.use((err, req, res, next) => {
+//   res.status(500).render('pages/internal-server-error', {
+//     layout: 'layouts/error-handling-layouts',
+//     title: 'Something Error!'
+//   })
+// });
 // # When error = 404 (Not Found)
-app.use((req, res, next) => {
-  res.status(404).render('pages/notfound', {
-    layout: 'layouts/error-handling-layouts',
-    title: 'Not Found!'
-  });
-});
+// app.use((req, res, next) => {
+//   res.status(404).render('pages/notfound', {
+//     layout: 'layouts/error-handling-layouts',
+//     title: 'Not Found!'
+//   });
+// });
 
 app.listen(port, () => {
   console.info(`${new Date()} | Express run in port ${port}`);
