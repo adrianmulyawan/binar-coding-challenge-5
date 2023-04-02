360feda462e937edd306cbea88015234cac8443f
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-04-02 13:44:01 +0700

    Tambah komentar pada logic controller menampilkan data user yang login dan edit user profile

--- controllers/api/profile.controller.js
+++ controllers/api/profile.controller.js
@@ -5,14 +5,19 @@ class ProfileController {
   // > Function to Show Profile User Login
   static showProfile = async (req, res) => {
     try {
-      console.info(req.user);
+      // console.info(req.user);
 
+      // > Destructuring id berdasarkan user yang login 
+      const { id } = req.user;
+
+      // > Cari profile dengan user_id = id user yang login
       const profile = await Profile.findOne({
         where: {
-          user_id: req.user.id
+          user_id: id
         }
       });
 
+      // > Bila data tidak ditemukan
       if (!profile) {
         return res.status(404).json({
           status: 'Failed',
@@ -21,6 +26,7 @@ class ProfileController {
         });
       }
 
+      // > Bila data ditemukan
       return res.status(200).json({
         status: 'Success',
         statusCode: 200,
@@ -40,12 +46,17 @@ class ProfileController {
   // > Function to Edit Profile User Login
   static editProfile = async (req, res) => {
     try {
+      // > Destructuring id dari user yang sedang login
+      const { id } = req.user;
+
+      // > Cari profile user (user_id) berdasarkan id user yang login
       const profile = await Profile.findOne({
         where: {
-          user_id: req.user.id
+          user_id: id
         }
       });
-
+      
+      // > Bila profile tidak ditemukan
       if (!profile) {
         return res.status(404).json({
           status: 'Failed',
@@ -54,13 +65,18 @@ class ProfileController {
         });
       }
 
+      // > Update profile
       const updateProfile = await profile.update({
-        fullname: req.body.fullname || profile.fullname,
+        // => terdapat 2 kondisi
+        // # req.body.namaBody : apabila user mengisikan data form yang akan diedit
+        // # profile.namaBody : apabila user tidak isi formnya, maka akan digunakan nilai lamanya (yang diambil dari db)
+        fullname: req.body.fullname || profile.fullname, 
         email: req.body.email || profile.email,
         address: req.body.address || profile.address,
         phone_number: req.body.phone_number || profile.phone_number,
       });
 
+      // > Sukses update data profile user
       return res.status(201).json({
         status: 'Success',
         statusCode: 201,
